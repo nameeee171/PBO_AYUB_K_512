d56919114871c2809c9ce53ed84aeea098c17c17
nameeee171 <nandaaaaa12567@gmail.com>
2025-03-04 19:04:50 -0800

    pwh

---
+++ Codelab/Modul2/latihan.java
@@ -0,0 +1,16 @@
+java until.Scanner;
+
+class main {
+    public static void mai(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String nama = "rizky";
+        int NIM = 508;
+        String admin = "Admin" + NIM;
+        String password = "password" +NIM;
+
+        System.out.print("Pilihan login : \n1.Admin\n2.Mahasiswa\nPilihan anda= ");
+        
+} {
+
+    
+}
