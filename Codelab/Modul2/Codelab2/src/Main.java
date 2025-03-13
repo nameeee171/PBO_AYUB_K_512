d13cc6cf8f6df9a162eef8653f2fbbaf3ad3a454
nameeee171 <nandaaaaa12567@gmail.com>
2025-03-13 07:03:33 -0700

    modul2

---
+++ Codelab/Modul2/Codelab2/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        RekeningBank rek1 = new RekeningBank();
+        RekeningBank rek2 = new RekeningBank();
+
+        rek1.nomor = "202410370110508";
+        rek1.nama = "lunox punya putri";
+        rek1.saldo = 2000000;
+
+        rek2.nomor = "202410370110480";
+        rek2.nama = "putri punya lunox";
+        rek2.saldo = 500000;
+
+        boolean ulang = true;
+
+        while (ulang){
+            System.out.println("============= Menu Transaksi ===========");
+            System.out.println("1.Setor Uang");
+            System.out.println("2.Tarik Tunai");
+            System.out.println("3.Keluar");
+            System.out.print("Pilihan  transaksi : ");
+            int pilihan = input.nextInt();
+
+            if (pilihan == 1){
+                System.out.print("Masukkan Nomor Rekening (1) " + rek1.nama + " dan (2) " + rek2.nama + " : ");
+                int choice = input.nextInt();
+                System.out.print("Masukkan Nominal Setor : ");
+                double Str = input.nextDouble();
+                if(choice == 1){
+                    rek1.setorUang(Str);
+                }else if (choice == 2){
+                    rek2.setorUang(Str);
+                }else{
+                    System.out.println("Pilihan Nommor Rekening Tidak Valid");
+                }
+            }else if (pilihan == 2){
+                System.out.print("Masukkan Nomor Rekening (1) " + rek1.nama + " dan (2) " + rek2.nama + " : ");
+                int choice2 = input.nextInt();
+                System.out.print("Masukkan Nominal Uang : ");
+                double Wd = input.nextDouble();
+
+                if(choice2 == 1){
+                    rek1.tarikTunai(Wd);
+                }else if(choice2 == 2){
+                    rek2.tarikTunai(Wd);
+                }else{
+                    System.out.println("Pilihan Nommor Rekening Tidak Valid");
+                }
+            }else if (pilihan == 3){
+                System.out.println("Terima Kasih Telah Menggunakan layanan Kami");
+                ulang = false;
+            }else{
+                System.out.println("Pilihan Transaksi Tidak Valid");
+            }
+        }
+        rek1.tampilkanInfo();
+        rek2.tampilkanInfo();
+
+        input.close();
+        }
+    }
--- Codelab/Modul2/Codelab2/src/RekeningBank.java
+++ Codelab/Modul2/Codelab2/src/RekeningBank.java
@@ -1,3 +1,30 @@
 public class RekeningBank {
-
-}
+    String nomor,nama;
+    double saldo;
+   
+    void tampilkanInfo(){
+        System.out.println("===============================");
+        System.out.println("nomor rekening : " + nomor);
+        System.out.println("nama pemilik : " + nama);
+        System.out.println("saldo : " +"Rp."+saldo);
+        System.out.println("===============================\n");
+    }
+   
+    void setorUang(double total){
+        if (total >0 ){
+            saldo += jumlah;
+            System.out.println(nama + " " + "menyetor Rp." + total + " " + "saldo sekarang Rp." + saldo);
+        }
+    }
+   
+    void tarikTunai(double total){
+        if(saldo >= jumlah){
+            saldo -= jumlah;
+            System.out.println(nama + " " + "menarik Rp." + total + " " + " (berhasil !) saldo saat ini Rp." + saldo );
+        }else{
+            System.out.println(nama + " " + "menarik Rp." + total + " " + "(gagal ! saldo anda tidak cukup)" + " " + "saldo saat ini Rp." + saldo );
+   
+        }
+    }
+   
+   }
